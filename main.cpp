f417a2e7366c3efefa1b857ed915848af97c3e29
James Elford <james.p.elford@googlemail.com>
2012-07-12 14:53:15 +0100

    New version of Sockets to include Address convenience class
    Also updated interface to take four commandline arguments. Usage:
    
        echoserver LISTEN_ADDR LISTEN_PORT TARGET_ADDR TARGET_PORT
    
    TODO: Write some tests. Test support for IPV6 (currently only
    IPV4 allowed; got socket errors)
    
    TODO: Find out why I got socket errors for IPV6

--- main.cpp
+++ main.cpp
@@ -30,108 +30,113 @@ void forward(Socket& incoming, Socket& destination, bool& round_over)
     cerr << incoming.identify() << "->" << destination.identify() << ": " << &tmp[0] << endl;
 }
 
-int main()
+int main(int const argc, char const * const argv[])
 {
-    // Address/port descriptions for incoming connection
-    in_addr_t listen_addr = htonl((((127 << 8) | 0) << 8 | 0) << 8 | 1);
-    in_port_t listen_port = htons(5955);
+    if (argc < 5)
+    {
+        cout << "Usage: " << endl << argv[0] << " LISTEN_ADDRESS LISTEN_PORT TARGET_ADDRESS TARGET_PORT " << endl;
+        return EXIT_FAILURE;
+    }
 
-    sockaddr_in listen_address;
-    memset(&listen_address, 0, sizeof(listen_address));
-
-    listen_address.sin_family = AF_INET;
-    listen_address.sin_addr.s_addr = listen_addr;
-    listen_address.sin_port = listen_port;
-
-    // Address/port descriptions for outgoing connection
-    in_addr_t target_addr = htonl((((127 << 8) | 0) << 8 | 0) << 8 | 1);
-    in_port_t target_port = htons(5905);
-
-    sockaddr_in target_address;
-    memset(&target_address, 0, sizeof(target_address));
-
-    target_address.sin_family = AF_INET;
-    target_address.sin_port = target_port;
-    target_address.sin_addr.s_addr = target_addr;
-
-    // Set up two tcp connections
-    Socket destination(PF_INET, SOCK_STREAM, 0);
-    Socket listen_socket(PF_INET, SOCK_STREAM, 0);
+    // Hints for building both listen & connection addresses
+    addrinfo hints;
+    ::memset(&hints, 0, sizeof(hints));
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_family = PF_INET;      // UNSPEC results in INET6, but our router doesn't assign INET6 addresses (EADDRNOTAVAIL)
 
     try
     {
+        Address listen_address = Address(argv[1], argv[2], hints);
+        Address target_address = Address(argv[3], argv[4], hints);
+    
 
-        // Set sockets to be non-blocking
-        listen_socket.set_nonblocking(true);
-        destination.set_nonblocking(true);
-        // Don't wait for timeout on listen port; just reuse it
-        listen_socket.set_reuse(true);
+        // Set up two tcp connections
+        Socket destination(target_address.family, SOCK_STREAM, 0);
+        Socket listen_socket(listen_address.family, SOCK_STREAM, 0);
 
-        // Bind on the listen port
-        listen_socket.bind_to(listen_address);
-
-        // Listen for incoming connections
-        cout << "Listening for incoming data" << endl;
-        listen_socket.listen(32);
-
-        // Enter the main loop
-        do
+        try
         {
-            // Block for an incoming connection
-            cout << "Awaiting incoming connection" << endl;
-            wait_for_read(&listen_socket);
-            Socket incoming = listen_socket.accept(NULL, NULL);
-            cout << "Accepted incoming connection" << endl;
 
-            // Establish connection to target 
-            cout << "Connecting to server..." << endl;
-            destination.connect(target_address); 
+            // Set sockets to be non-blocking
+            listen_socket.set_nonblocking(true);
+            destination.set_nonblocking(true);
+            // Don't wait for timeout on listen port; just reuse it
+            listen_socket.set_reuse(true);
 
+            // Bind on the listen port
+            listen_socket.bind_to(listen_address);
 
-            // We select on vectors of sockets
-            vector<const Socket*> sockets;
-            sockets.push_back(&destination);
-            sockets.push_back(&incoming);
+            // Listen for incoming connections
+            cout << "Listening for incoming data" << endl;
+            listen_socket.listen(32);
 
-            // Ready to pipe data
-            bool round_over = false;
+            // The main loop just awaits incoming connections, forwards
+            // connects to the destination, forwards any traffic, then
+            // discards the socket and awaits new incoming connections.
             do
             {
-                cerr << "doing select for read data" << endl;
-                auto readable = select_for_reading(sockets);
+                // Block for an incoming connection
+                cout << "Awaiting incoming connection" << endl;
+                wait_for_read(&listen_socket);
+                Socket incoming = listen_socket.accept(NULL, NULL);
+                cout << "Accepted incoming connection" << endl;
 
-                for (auto r_socket : readable)
+                // Establish connection to target 
+                cout << "Connecting to server..." << endl;
+                destination.connect(target_address); 
+
+
+                // We select on vectors of sockets
+                vector<const Socket*> sockets;
+                sockets.push_back(&destination);
+                sockets.push_back(&incoming);
+
+                // Ready to pipe data
+                bool round_over = false;
+                do
                 {
-                    // Get pointers to sockets back
-                    if (r_socket == &incoming)
-                    {
-                        forward(incoming, destination, round_over);
-                    }
-                    else if (r_socket == &destination)
-                    {
-                        forward(destination, incoming, round_over);
-                    }
-                    else
-                        cerr << "Unknown socket found in read set!" << endl;
-                }
+                    cerr << "doing select for read data" << endl;
+                    auto readable = select_for_reading(sockets);
 
-            } while (!round_over);
+                    for (auto r_socket : readable)
+                    {
+                        // Get pointers to sockets back
+                        if (r_socket == &incoming)
+                        {
+                            forward(incoming, destination, round_over);
+                        }
+                        else if (r_socket == &destination)
+                        {
+                            forward(destination, incoming, round_over);
+                        }
+                        else
+                            cerr << "Unknown socket found in read set!" << endl;
+                    }
 
-            // Set up a new Socket to connect to target - old one is dead.
-            destination = Socket(PF_INET, SOCK_STREAM, 0);
-        } while (true); // Until termination
-    }
-    catch (unique_ptr<SocketException>&& e)
-    {
-        const Socket* s = e->retrieve_socket();
-        if (s != NULL)
-        {
-            cerr << "Problem in Socket " << s->identify() << ": ";
-            if (s->is_listening())
-                cerr << "[Listening] ";
-            else
-                cerr << "[Not Listening] ";
+                } while (!round_over);
+
+                // Set up a new Socket to connect to target - old one is dead.
+                destination = Socket(PF_INET, SOCK_STREAM, 0);
+            } while (true); // Until termination
         }
-        cerr << e->what() << endl;
+        catch (unique_ptr<SocketException>&& e)
+        {
+            const Socket* s = e->retrieve_socket();
+            if (s != NULL)
+            {
+                cerr << "Problem in Socket " << s->identify() << ": ";
+                if (s->is_listening())
+                    cerr << "[Listening] ";
+                else
+                    cerr << "[Not Listening] ";
+            }
+            cerr << e->what() << endl;
+        }
     }
+    catch(AddressException e)
+    {
+        cerr << "Address exception: " << e.msg() << endl;
+        return EXIT_FAILURE;
+    }
+
 }
